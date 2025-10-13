da767c48870975fc88fc3f793cfeff0a0a761ede
arifinkhoirul <irul30582@gmail.com>
2025-10-13 16:44:06 +0700

    sudah consume bagian maincard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MainCard;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function homepage() {
-        return view('user.homepage');
+    $dataMainCardYear = MainCard::where('title', 'Years Of Experience')->first();
+    $dataMainCardClient = MainCard::where('title', 'Clients')->first();
+    $dataMainCardPositive = MainCard::where('title', 'Positive Feedback')->first();
+
+    // dd($dataMainCardYear->title);
+
+        return view('user.homepage', compact('dataMainCardClient', 'dataMainCardYear', 'dataMainCardPositive'));
     }
 }
