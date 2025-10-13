8f5964434fbdce03b30a90604bb36cee7afe433e
arifinkhoirul <irul30582@gmail.com>
2025-10-13 23:11:02 +0700

    done lempar data ke homepage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\MainCard;
+use App\Models\Price;
+use App\Models\Question;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function homepage() {
-    $dataMainCardYear = MainCard::where('title', 'Years Of Experience')->first();
-    $dataMainCardClient = MainCard::where('title', 'Clients')->first();
-    $dataMainCardPositive = MainCard::where('title', 'Positive Feedback')->first();
+    public function homepage()
+    {
+        $dataMainCardYear = MainCard::where('title', 'Years Of Experience')->first();
+        $dataMainCardClient = MainCard::where('title', 'Clients')->first();
+        $dataMainCardPositive = MainCard::where('title', 'Positive Feedback')->first();
 
-    // dd($dataMainCardYear->title);
+        $testimonials = Testimonial::all();
 
-        return view('user.homepage', compact('dataMainCardClient', 'dataMainCardYear', 'dataMainCardPositive'));
+        $dataPriceStarter = Price::where('price', '1000000')->first();
+        $dataPriceProfesional = Price::where('price', '2000000')->first();
+        $dataPriceEnterprice = Price::where('price', '3000000')->first();
+        // $dataPriceStarter = Price::first();
+        // dd($dataPriceStarter->features->feature);
+
+        $questions = Question::all();
+
+        return view('user.homepage', compact('dataMainCardClient', 'dataMainCardYear', 'dataMainCardPositive', 'testimonials',     'dataPriceStarter', 'dataPriceProfesional', 'dataPriceEnterprice', 'questions'));
     }
 }
